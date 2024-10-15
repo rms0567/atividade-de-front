efb2bf477bbf7c115d50bd5c66581f156fe0d715
@rosa_maris1 <163297040+rms0567@users.noreply.github.com>
2024-10-15 08:30:39 -0300

    cor

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 document.getElementById('form').addEventListener('submit', function(e) {
-    e.preventDefault(); // Evita o envio padrão do formulário
+    e.preventDefault(); // formulário
 
-    const name = document.getElementById('name').value; // Obtém o nome do input
-    const email = document.getElementById('email').value; // Obtém o email do input
+    const name = document.getElementById('name').value; // nome do input
+    const email = document.getElementById('email').value; //  email do input
 
-    // Cria uma nova linha na tabela
+    //  nova linha na tabela
     const table = document.getElementById('table').querySelector('tbody');
     const row = document.createElement('tr');
     
-    // Adiciona células à nova linha
+    // células a nova linha
     const nameCell = document.createElement('td');
     nameCell.textContent = name; // Define o conteúdo da célula como o nome
     const emailCell = document.createElement('td');
     emailCell.textContent = email; // Define o conteúdo da célula como o email
     
-    // Anexa as células à linha
+    // as células à linha
     row.appendChild(nameCell);
     row.appendChild(emailCell);
     
-    // Anexa a linha ao corpo da tabela
+    // coloca a linha no corpo da tabela
     table.appendChild(row);
     
     // Limpa os inputs
